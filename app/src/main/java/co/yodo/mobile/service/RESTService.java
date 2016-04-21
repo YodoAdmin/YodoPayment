0ba01d9fd84ae3a8e678099cd063f91f81964a87
Luis Talavera <luis@yodopay.com>
2016-04-21 14:04:34 -0500

    Production 21/04

--- app/src/main/java/co/yodo/mobile/service/RESTService.java
+++ app/src/main/java/co/yodo/mobile/service/RESTService.java
@@ -30,8 +30,8 @@ public class RESTService extends IntentService {
     private static final String TAG = RESTService.class.getSimpleName();
 
     /** Switch server IP address */
-    //private static final String IP 	         = "http://50.56.180.133";  // Production
-    private static final String IP 			 = "http://198.101.209.120";  // Development
+    private static final String IP 	         = "http://50.56.180.133";  // Production
+    //private static final String IP 			 = "http://198.101.209.120";  // Development
     private static final String YODO_ADDRESS = "/yodo/yodoswitchrequest/getRequest/";
 
     /** It is the ID of the application (the package) used for the extras */
